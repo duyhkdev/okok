c5e3003be13aadd1fcb381bf725827b8fe96aa60
hoangkimduy2003 <duy0976541482@gmail.com>
2024-09-30 16:52:48 +0700

    :demo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ok");
+        System.out.println(TrangThai.DANGCHO.status == 1l);
     }
 }
---
+++ src/TrangThai.java
@@ -0,0 +1,13 @@
+public enum TrangThai {
+    DANGCHO(1),
+    DANGTHU(2),
+    HOANTHANH(3);
+
+    int status;
+
+    TrangThai(int status) {
+        this.status = status;
+    }
+
+
+}
